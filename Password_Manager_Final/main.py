8dd983a70b72b9c56c9d2e74838c22ae1288081e
Abdurahim Hudulov <abul.hudul@gmail.com>
2024-01-19 17:21:25 +0100

    Updated

---
+++ Password_Manager_Final/main.py
@@ -0,0 +1,110 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+import json
+
+# ---------------------------- CONSTANTS ------------------------------- #
+DEFAULT_EMAIL = "abul.hudul@gmail.com"
+DATA_FILE = "data.json"
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+	letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	numbers = '0123456789'
+	symbols = '!#$%&()*+'
+
+	password_letters = [choice(letters) for _ in range(randint(8, 10))]
+	password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+	password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+	password_list = password_letters + password_symbols + password_numbers
+	shuffle(password_list)
+
+	password = "".join(password_list)
+	password_entry.insert(0, password)
+	pyperclip.copy(password)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+	website = website_entry.get()
+	email = email_entry.get()
+	password = password_entry.get()
+	new_data = {
+		website: {
+			"email": email,
+			"password": password
+		}
+	}
+
+	if len(website) == 0 or len(password) == 0:
+		messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+		return
+
+	try:
+		with open(DATA_FILE, "r") as data_file:
+			data = json.load(data_file)
+	except (FileNotFoundError, json.JSONDecodeError):
+		data = {}
+
+	data.update(new_data)
+	with open(DATA_FILE, "w") as data_file:
+		json.dump(data, data_file, indent=4)
+
+	website_entry.delete(0, END)
+	password_entry.delete(0, END)
+
+# ---------------------------- Password Finder ------------------------------- #
+def find_password():
+	website = website_entry.get()
+	try:
+		with open(DATA_FILE, "r") as data_file:
+			data = json.load(data_file)
+	except FileNotFoundError:
+		messagebox.showerror(title="File Error", message="No Data File Found.")
+		return
+
+	if website in data:
+		email = data[website]['email']
+		password = data[website]['password']
+		messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+	else:
+		messagebox.showinfo(title="Error", message=f"No details for the website {website} exists.")
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+#Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+#Entries
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1, columnspan=1)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, DEFAULT_EMAIL)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
+
+window.mainloop()
---
+++ day_31/main.py
@@ -0,0 +1,4 @@
+try:
+    file = open('output.txt')
+except:
+    open('output.txt', 'w').close()
